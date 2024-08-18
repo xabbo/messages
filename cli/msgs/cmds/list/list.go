8013e3955814b91edc7a1afb555ecfa4223ad0ce
rob <rob@b7c.io>
2024-08-18 17:40:07 +1200

    add json input/prefix/clean options to list command

--- cli/msgs/cmds/list/list.go
+++ cli/msgs/cmds/list/list.go
@@ -1,9 +1,9 @@
 package list
 
 import (
+	"encoding/json"
 	"fmt"
 	"slices"
-	"strings"
 
 	"github.com/spf13/cobra"
 
@@ -20,68 +20,97 @@ var listCmd = &cobra.Command{
 }
 
 var opts struct {
-	release string
+	release   string
+	variant   string
+	direction string
+	extract   bool
+	prefix    bool
+	clean     bool
 }
 
 func init() {
 	root.AddCommand(listCmd)
 
+	listCmd.Flags().StringVarP(&opts.direction, "direction", "d", "both", "Direction of messages to fetch (in/out/both)")
+	listCmd.Flags().StringVarP(&opts.variant, "variant", "v", "", "Variant to fetch messages for")
 	listCmd.Flags().StringVarP(&opts.release, "release", "r", "", "Release to fetch messages for (defaults to latest release)")
+	listCmd.Flags().BoolVarP(&opts.extract, "extract", "x", false, "Extract message names from JSON input")
+	listCmd.Flags().BoolVar(&opts.prefix, "prefix", false, "Prefix message names with their direction")
+	listCmd.Flags().BoolVar(&opts.clean, "clean", true, "Clean message names by removing the (Message)Event/Composer suffix")
 }
 
 func run(cmd *cobra.Command, args []string) (err error) {
 	cmd.SilenceUsage = true
 
-	var direction messages.Direction
+	var directions []messages.Direction
 	switch args[0] {
 	case "in":
-		direction = messages.In
+		directions = []messages.Direction{messages.In}
 	case "out":
-		direction = messages.Out
+		directions = []messages.Direction{messages.In}
+	case "both":
+		directions = []messages.Direction{messages.In, messages.Out}
 	default:
 		return fmt.Errorf("invalid direction: %s", args[0])
 	}
 
-	variant := args[1]
-	version := opts.release
-
-	if version == "" {
-		cmd.PrintErrf("Fetching latest release for %s...\n", variant)
-		releases, err := sulek.FetchReleases(variant)
+	var sulekMessages sulek.Messages
+	if opts.extract {
+		err = json.NewDecoder(cmd.InOrStdin()).Decode(&sulekMessages)
 		if err != nil {
-			return err
+			return
 		}
-		if len(releases) == 0 {
-			return fmt.Errorf("no releases found")
-		}
-		version = releases[0].Version
-	}
-
-	cmd.PrintErrf("Fetching messages for %s version %s\n", variant, version)
-	allMsgs, err := sulek.FetchMessages(variant, version)
-	if err != nil {
-		return
-	}
-
-	var msgs []sulek.Message
-	if direction == messages.In {
-		msgs = allMsgs.Incoming
 	} else {
-		msgs = allMsgs.Outgoing
+		variant := args[1]
+		version := opts.release
+
+		if version == "" {
+			cmd.PrintErrf("Fetching latest release for %s...\n", variant)
+			releases, err := sulek.FetchReleases(variant)
+			if err != nil {
+				return err
+			}
+			if len(releases) == 0 {
+				return fmt.Errorf("no releases found")
+			}
+			version = releases[0].Version
+		}
+
+		cmd.PrintErrf("Fetching messages for %s version %s\n", variant, version)
+		sulekMessages, err = sulek.FetchMessages(variant, version)
+		if err != nil {
+			return
+		}
 	}
 
 	messageNames := []string{}
-	for _, msg := range msgs {
-		name := msg.Name
-		if !strings.Contains(variant, "shockwave") {
-			name = messages.CleanName(direction, name)
+	for _, dir := range directions {
+		var msgs []sulek.Message
+		switch dir {
+		case messages.In:
+			msgs = sulekMessages.Incoming
+		case messages.Out:
+			msgs = sulekMessages.Outgoing
+		default:
+			continue
 		}
-		messageNames = append(messageNames, name)
+
+		for _, msg := range msgs {
+			name := msg.Name
+			if opts.clean {
+				name = messages.CleanName(dir, msg.Name)
+			}
+			if opts.prefix {
+				name = dir.Short() + ":" + name
+			}
+			messageNames = append(messageNames, name)
+		}
 	}
 
 	slices.Sort(messageNames)
 	for _, name := range messageNames {
 		cmd.Println(name)
 	}
+
 	return
 }
